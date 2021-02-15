1b4fc6dbb33f40a9d03c634c9d39e02a47edf258
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-15 11:58:58 -0500

    order reducer made

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import { Container, Row, Col } from "react-bootstrap";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProfileScreen from "./screens/ProfileScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} exact />
+          <Route path="/placeorder" component={PlaceOrderScreen} exact />
+          <Route path="/payment" component={PaymentScreen} exact />
+          <Route path="/shipping" component={ShippingScreen} exact />
+          <Route path="/register" component={RegisterScreen} exact />
+          <Route path="/profile" component={ProfileScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,50 @@ import {
   Button,
   Container,
 } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link className="fas fa-shopping-cart" href="#/cart">
-                Cart
-              </Nav.Link>
-              <Nav.Link href="#login" className="fas fa-user">
-                Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="fas fa-shopping-cart" href="#/cart">
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link href="#login" className="fas fa-user">
+                    Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
